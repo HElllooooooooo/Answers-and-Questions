59739548712c7786ee990bf7686be637b391e4dc
HElllooooooooo <93811333+HElllooooooooo@users.noreply.github.com>
2021-11-08 08:24:40 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let btns = document.querySelectorAll('.wrap-toggle');
+
+let wraps = document.querySelectorAll('.wrap');
+
+
+// for (let x = 0; x < btns.length; x++) {
+//     btns[x].addEventListener('click', function () {
+//         wraps[x].classList.toggle('active');
+//     })
+// }
+
+for (let x = 0; x <btns.length; x++) {
+    btns[x].addEventListener('click', function () {
+        wraps[x].classList.toggle('active');
+    })
+}
